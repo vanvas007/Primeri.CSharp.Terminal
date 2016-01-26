82221932ce2fa969366182e5c421825a6254599d
Ivan Vasilev <vanvas62@gmail.com>
2016-01-26 22:22:24 +0200

    Модул 02 - 01 Добавяне на коментари

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+//			ДЕФИНИРАНЕ НА ПРОМЕНЛИВИ
+
+//			събиране на числа
+
+//			писане в конзолата
+		}
+	}
+}
