eb36d70907a515ad63e5edc2e9910f45896e4a9d
Ivan Vasilev <vanvas62@gmail.com>
2016-01-27 21:58:41 +0200

    Модул 02, Видео 02

--- Modul02/Modul02/Program.cs
+++ Modul02/Modul02/Program.cs
@@ -8,10 +8,20 @@ namespace Modul02
 		{
 			
 //			ДЕФИНИРАНЕ НА ПРОМЕНЛИВИ
-
+			int a =0, b=0;
+			int sum = 0;
 //			събиране на числа
+			a=5;
+			b=10;
+
+			sum = a + b;
 
 //			писане в конзолата
+			Console.Write (a);
+			Console.Write ("+");
+			Console.Write (b);
+			Console.Write ("=");
+			Console.WriteLine (sum);
 		}
 	}
 }
