147bb56a15f46205a866f5243460866fa2dc8930
Ivan Vasilev <vanvas62@gmail.com>
2016-02-01 16:13:39 +0200

    Модул 03, видео О6 - работа с условно оператори

--- Modul03/BullExplorer/Program.cs
+++ Modul03/BullExplorer/Program.cs
@@ -9,16 +9,27 @@ namespace BullExplorer
 			//bool _b = true; //true/false
 			//bool _test = 5 > 10;
 			int[] _i = new int[] { 4, 3, 2 };
-			int _ui = Convert.ToInt32(Console.ReadLine());
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 =_ui > 0;
-			bool _exp01 = _check01 && _check02; // логическо И "&&"
-			bool _exp02 = _check01 || _check02; //логическо ИЛИ "||"
-			bool _exp03 = ! _check01; //логическо отрицание "! "
-			Console.WriteLine ("Логическо И: " +_exp01.ToString());
-			Console.WriteLine ("Логическо ИЛИ: " +_exp02.ToString());
-			Console.WriteLine ("Логическо отрицание на " +_check01.ToString() +": "+_exp03.ToString ());
-			// Console.WriteLine (_i[_ui - 1]);
+			int _ui = Convert.ToInt32 (Console.ReadLine ());
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 =_ui > 0;
+//			bool _exp01 = _check01 && _check02; // логическо И "&&"
+//			bool _exp02 = _check01 || _check02; //логическо ИЛИ "||"
+//			bool _exp03 = ! _check01; //логическо отрицание "! "
+//			Console.WriteLine ("Логическо И: " +_exp01.ToString());
+//			Console.WriteLine ("Логическо ИЛИ: " +_exp02.ToString());
+//			Console.WriteLine ("Логическо отрицание на " +_check01.ToString() +": "+_exp03.ToString ());
+			if (_ui > 0 && _ui <= _i.Length) {
+				Console.WriteLine (_i [_ui - 1]);
+			} else {
+				if(_ui > _i.Length)
+				{Console.WriteLine ("Индекса е прекалено голям\n\n");
+				}	if(_ui <= 0)
+				{Console.WriteLine ("Индекса е прекалено малък\n\n");
+				}
+
+
+			}
 		}
-	}
-}
+
+	}}
+
