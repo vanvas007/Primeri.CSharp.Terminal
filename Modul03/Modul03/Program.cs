40cde955db910e66303cbddc86ed57734aaa68d4
Ivan Vasilev <vanvas62@gmail.com>
2016-02-01 13:56:53 +0200

    Модул 03, видео 03

--- Modul03/Modul03/Program.cs
+++ Modul03/Modul03/Program.cs
@@ -5,11 +5,22 @@ namespace Modul03
 	class MainClass
 	{
 		public static void Main (string[] args)
-		{double a = 0, b = 0;
+		{double a = 0, b = 0, c = 0;
 			Console.Write ("> a = "); a = Convert.ToDouble(Console.ReadLine ());
 			Console.Write ("> b = "); b = Convert.ToDouble(Console.ReadLine ());
+			//Форматиране с дименсия
 			String _format = "#0.00 м3";
-			Console.WriteLine ("\n\n >a /b " +(a / b).ToString( _format));
+			//Степенуване
+			c = Math.Pow (a , b);
+			Console.WriteLine ("\n\n >a ^ b " + c.ToString( _format));
+			//Коренуване
+			c = Math.Sqrt (b);
+			Console.WriteLine ("\n\n >b^1/2 " + c.ToString( _format));
+			//Закръгления
+			c = Math.Round (a / b);
+			Console.WriteLine ("\n\n >(a / b) " + c.ToString());
+
+
 		}
 	}
 }
