b1c86d7864c04166f2d7ed571540b9c7e53cf6c3
Ivan Vasilev <vanvas62@gmail.com>
2016-02-01 17:42:12 +0200

    Модул 03, видео 07 - примери тип Boll

--- Modul03/BullExplorer/Program.cs
+++ Modul03/BullExplorer/Program.cs
@@ -9,27 +9,54 @@ namespace BullExplorer
 			//bool _b = true; //true/false
 			//bool _test = 5 > 10;
 			int[] _i = new int[] { 4, 3, 2 };
-			int _ui = Convert.ToInt32 (Console.ReadLine ());
-//			bool _check01 = _ui <= _i.Length;
-//			bool _check02 =_ui > 0;
+			int _ui = 0; //Convert.ToInt32 (Console.ReadLine ());
+			string _input = "0";
+			_input = Console.ReadLine ();
+			Console.Write ("Моля въведете индекс: ");
+			bool _check01 = int.TryParse (_input, out _ui);
+			bool _check02 = _ui <= _i.Length;
+			bool _check03 = _ui > 0;
+			if (_check01 && _check02 && _check03) {
+				Console.WriteLine (_i [_ui - 1]);
+				
+			}
+			if (!_check01) {
+				Console.WriteLine ("Индекса трябва да бъде цяло число\n\n");
+				
+			
+				if (!_check02) {
+					Console.WriteLine ("Индекса е прекалено голям\n\n");
+
+				}
+			}
+			if (!_check03 && _check01) {
+				Console.WriteLine ("Индекса е прекалено малък\n\n");
+			}
+		}
+
+	}
+}
+
+
+
 //			bool _exp01 = _check01 && _check02; // логическо И "&&"
 //			bool _exp02 = _check01 || _check02; //логическо ИЛИ "||"
 //			bool _exp03 = ! _check01; //логическо отрицание "! "
 //			Console.WriteLine ("Логическо И: " +_exp01.ToString());
 //			Console.WriteLine ("Логическо ИЛИ: " +_exp02.ToString());
 //			Console.WriteLine ("Логическо отрицание на " +_check01.ToString() +": "+_exp03.ToString ());
-			if (_ui > 0 && _ui <= _i.Length) {
-				Console.WriteLine (_i [_ui - 1]);
-			} else {
-				if(_ui > _i.Length)
-				{Console.WriteLine ("Индекса е прекалено голям\n\n");
-				}	if(_ui <= 0)
-				{Console.WriteLine ("Индекса е прекалено малък\n\n");
-				}
+//			if (_ui > 0 && _ui <= _i.Length) {
+//				Console.WriteLine (_i [_ui - 1]);
+//			} else {
+//				if(_ui > _i.Length)
+//				{Console.WriteLine ("Индекса е прекалено голям\n\n");
+//				}	if(_ui <= 0)
+//				{Console.WriteLine ("Индекса е прекалено малък\n\n");
+//				}
 
 
-			}
-		}
-
-	}}
-
+//			}
+//		}
+//
+//	}}
+//
