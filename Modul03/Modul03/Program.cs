85be0b00d1690fd5844a85b288d76bb47d0980c3
Ivan Vasilev <vanvas62@gmail.com>
2016-02-01 13:38:28 +0200

    Модул 03, видео 02 - форматиране на дробни числа

---
+++ Modul03/Modul03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Modul03
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{double a = 0, b = 0;
+			Console.Write ("> a = "); a = Convert.ToDouble(Console.ReadLine ());
+			Console.Write ("> b = "); b = Convert.ToDouble(Console.ReadLine ());
+			String _format = "#0.00 м3";
+			Console.WriteLine ("\n\n >a /b " +(a / b).ToString( _format));
+		}
+	}
+}
