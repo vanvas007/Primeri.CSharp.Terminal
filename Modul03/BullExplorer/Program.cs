0425fc93519109bba5dedc7c9d2d5373131088a1
Ivan Vasilev <vanvas62@gmail.com>
2016-02-02 11:00:20 +0200

    Модул 03, видео 08 - Условно присвояване

--- Modul03/BullExplorer/Program.cs
+++ Modul03/BullExplorer/Program.cs
@@ -11,23 +11,25 @@ namespace BullExplorer
 			int[] _i = new int[] { 4, 3, 2 };
 			int _ui = 0; //Convert.ToInt32 (Console.ReadLine ());
 			string _input = "0";
-			_input = Console.ReadLine ();
 			Console.Write ("Моля въведете индекс: ");
+			_input = Console.ReadLine ();
+
 			bool _check01 = int.TryParse (_input, out _ui);
 			bool _check02 = _ui <= _i.Length;
 			bool _check03 = _ui > 0;
+
 			if (_check01 && _check02 && _check03) {
 				Console.WriteLine (_i [_ui - 1]);
 				
 			}
 			if (!_check01) {
 				Console.WriteLine ("Индекса трябва да бъде цяло число\n\n");
-				
+			}
 			
 				if (!_check02) {
 					Console.WriteLine ("Индекса е прекалено голям\n\n");
 
-				}
+
 			}
 			if (!_check03 && _check01) {
 				Console.WriteLine ("Индекса е прекалено малък\n\n");
