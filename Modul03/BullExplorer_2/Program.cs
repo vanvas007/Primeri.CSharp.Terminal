505737139ef3641340a539599f8cb016a7179086
Ivan Vasilev <vanvas62@gmail.com>
2016-02-02 12:08:34 +0200

    Модул 03, видео 10 - управление на грешки -Try ...Catch

--- Modul03/BullExplorer_2/Program.cs
+++ Modul03/BullExplorer_2/Program.cs
@@ -11,37 +11,49 @@ namespace BullExplorer_2
 			string _input = "0";
 			Console.Write ("Моля въведете индекс: ");
 			_input = Console.ReadLine ();
-
 			bool _check01 = int.TryParse (_input, out _ui);
+			try {
+				Console.Write ("Елемент "+ _ui.ToString() + "от масива е:");
+				Console.WriteLine (_i [_ui -1]);
+			}catch{Console.WriteLine ("<Недефиниран елемент>");
+			}
+
+			//Използване на основно присвояване
+
+
+
+
 //			_ui =(_ui <=_i.Length) ?_ui: 3;
 //			_ui =(_ui > 0) ? _ui: 1;
-			switch (_ui) {
-			case 1:
-				{
-					Console.Write ("1-ви елемент от масива:");
-					Console.WriteLine (_i [_ui-1]);
-					break;		
-				}
-				case 2:
-			{
-				Console.Write ("2-ри елемент от масива:");
-				Console.WriteLine (_i [_ui-1]);
-				break;
-			} 
-			case 3:
-			{
-				Console.Write ("3-ти елемент от масива:");
-				Console.WriteLine (_i [_ui-1]);
-				break;
-			} 
-			default:
-			{
-				Console.Write ("В масива няма стойност за този индекс.\nОпитайте с индекс между 1 и 3!\n\n");
-				break;
-			}
+
+//			//Използване на Switch
+//			switch (_ui) {
+//			case 1:
+//				{
+//					Console.Write ("1-ви елемент от масива:");
+//					Console.WriteLine (_i [_ui-1]);
+//					break;		
+//				}
+//				case 2:
+//			{
+//				Console.Write ("2-ри елемент от масива:");
+//				Console.WriteLine (_i [_ui-1]);
+//				break;
+//			} 
+//			case 3:
+//			{
+//				Console.Write ("3-ти елемент от масива:");
+//				Console.WriteLine (_i [_ui-1]);
+//				break;
+//			} 
+//			default:
+//			{
+//				Console.Write ("В масива няма стойност за този индекс.\nОпитайте с индекс между 1 и 3!\n\n");
+//				break;
+//			}
 
 //			if (_check01 ) {
 //				Console.WriteLine (_i [_ui - 1]);
 		}
 	}
-}}
+}
