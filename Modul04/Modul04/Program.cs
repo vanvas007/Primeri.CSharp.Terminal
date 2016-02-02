1f241ca67d5348cd50a7d5b7acdb4b2fd948e591
Ivan Vasilev <vanvas62@gmail.com>
2016-02-02 14:51:15 +0200

    Модул 04, видео 1 - работа с цикъл FOR

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			for(int i = 0; i <= 5; i+=2)
+			{
+				Console.WriteLine (i.ToString ());	//код на цикъла
+			}
+		}
+	}
+}
