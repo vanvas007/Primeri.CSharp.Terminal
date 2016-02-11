364f108042474917b89cf896298502f4c924d7be
i.vasilev <vanvas62@gmail.com>
2016-02-11 16:04:44 +0200

    Модул 04, видео - 07 - Примери с List

--- Modul04/ListExplorer/Program.cs
+++ Modul04/ListExplorer/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace ListExplorer
 {
 	class MainClass
@@ -8,8 +8,13 @@ namespace ListExplorer
 		{
 			//Дефиниции
 			string _userinput = "";
-			//List
-			//Потребителски код
+			List<int> _list = new List <int> (); 
+			_list.Add (5);
+			_list.Add (3);
+			_list.Add (9);
+
+
+			//Потребителски вход
 			do{
 				Console.Write ("? ");
 				_userinput=	Console.ReadLine ();
@@ -17,9 +22,30 @@ namespace ListExplorer
 				// Добавяне на стойности
 
 				//Преглед на List
+				if (_userinput.Contains ("show") )
+				{
+					Console.Write ("_list: " );
 
+					for (int i = 0; i < _list.Count; i++)
+					{			
+
+						
+						Console.Write (_list[i]);
+
+							if (i != _list.Count - 1) Console.Write (", ");
+					}
+					Console.WriteLine ();
+				}
 				//Размер на List
-			}while(_userinput !="exit");
+				} while(_userinput !="exit");
+			
 		}
 	}
+		  
 }
+		
+		
+	
+	
+	
+
