f0a507d82fab82547b9a61a2066efa0db1d91ccc
i.vasilev <vanvas62@gmail.com>
2016-02-26 14:54:13 +0200

    Модул05, Видео6 - Дефиниране на методи в клас

--- Modul05/ClasExplorer/Program.cs
+++ Modul05/ClasExplorer/Program.cs
@@ -5,21 +5,35 @@ namespace ClasExplorer
 
 	public class person
 	{
-		public string simpleString = "тестова променлива от клас";
-		public string user = "";
+		private string simpleString = "тестова променлива от клас";
+		private string user = "";
 		private string hiddenString = ""; 
 		public  person (string _ini)
-		{
-			if (_ini == "password1") hiddenString = "Потребител 1";
-			if (_ini == "password2") hiddenString = "Потребител 2";
+		{			
+			getUser (_ini);
+			setWelcomeText ();
 
-			user = hiddenString;
-				
-			if (hiddenString.Length > 0) simpleString = "Добре дошли отново,"+ user +"!" ;
-			else simpleString = "Вие нямата достъп до тазо програма";
 			//simpleString ="Промяня на променлива при инициализиране";
 			
 		}
+
+
+		private void getUser(string _ini)
+		{
+			if (_ini == "password1") hiddenString = "Потребител 1";
+			if (_ini == "password2") hiddenString = "Потребител 2";
+			user = hiddenString;
+		}
+		private void setWelcomeText ()
+		{
+			if (hiddenString.Length > 0) simpleString = "Добре дошли отново,"+ user +"!" ;
+			else simpleString = "Вие нямата достъп до тазо програма";
+		}
+		public string getWelcomeMessage()
+		{
+			return simpleString;
+		}
+
 
 	}
 	class MainClass
@@ -33,9 +47,9 @@ namespace ClasExplorer
 			person _person = new person (_user);
 
 			//Достъпвяне на клас
-			Console.WriteLine (_person.simpleString);
-			//_person.simpleString;
-			//_person.user;
+			Console.WriteLine (_person.getWelcomeMessage());
+			//_person.simpleString;//достъпва се, защото е public
+			//_person.user; 		//достъпва се, защото е public
 			//_person.hideenString // не може да се дистъпи, защото е privet 
 		}
 	}
